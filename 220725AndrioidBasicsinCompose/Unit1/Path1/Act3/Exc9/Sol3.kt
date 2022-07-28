3dac2a32c521f484a6eb870287bb37300fd93ee5
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-07-28 15:48:19 +0600

    Create Sol3.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act3/Exc9/Sol3.kt
@@ -0,0 +1,18 @@
+/*
+   Program Code
+*/
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+// Output
+
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
