c297aba2ffd560c97f09d5c5552b5ce039c24dfe
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:36:26 +0600

    Create Exc12.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc12.kt
@@ -0,0 +1,15 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val nextMeeting = "Next meeting is: "
+    val date = "January 1"
+    val reminder = nextMeeting + date
+    println(reminder)
+}
+
+// Output
+
+// Next meeting is: January 1
