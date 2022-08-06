299e5d04aa62b9df064cd06773e7852c9c9840b2
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:32:43 +0600

    Create Exc10.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc10.kt
@@ -0,0 +1,16 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = trip1 + trip2 + trip3
+    println("$totalTripLength miles left to destination")
+}
+
+// Output
+
+// 9.02 miles left to destination
