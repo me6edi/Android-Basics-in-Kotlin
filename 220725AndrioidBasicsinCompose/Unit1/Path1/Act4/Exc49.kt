b98699adc0870155ca02c7465162a5cc1a40f779
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:30:47 +0600

    Create Exc49.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc49.kt
@@ -0,0 +1,15 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count--
+    println("You have $count unread messages.")
+}
+
+// Output
+
+// You have 10 unread messages.
+// You have 9 unread messages.
