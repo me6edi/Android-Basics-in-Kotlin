bdb519c555af25b7bf1fc586e263a3ecb192595c
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:38:37 +0600

    Create Exc14.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc14.kt
@@ -0,0 +1,12 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+ 
+fun main() {
+    val notificationsEnabled: Boolean = true
+    println(notificationsEnabled)
+}
+
+// Output
+// true
