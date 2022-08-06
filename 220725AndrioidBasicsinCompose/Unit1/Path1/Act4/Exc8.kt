ff74e0d0f930310cf9a48f936246479a48e3718d
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:28:03 +0600

    Create Exc8.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc8.kt
@@ -0,0 +1,16 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    var count = 10
+    println("You have $count unread messages.")
+    count = count + 1
+    println("You have $count unread messages.")
+}
+
+// Output
+
+// You have 10 unread messages.
+// You have 11 unread messages.
