837a3eaa3ce88e7604dc8f36ed0f3913f7ea117a
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:16:04 +0600

    Create Exc5.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc5.kt
@@ -0,0 +1,18 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos")
+    println("$photosDeleted photos deleted")
+    println("${numberOfPhotos - photosDeleted} photos left")
+}}
+
+// Output
+
+// 100 photos
+10 photos deleted
+90 photos left
