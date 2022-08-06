f8ba63abf826817855a6c65654012fb31dc07e04
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:14:46 +0600

    Create Exc4.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc4.kt
@@ -0,0 +1,14 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val unreadCount = 5
+    val readCount = 100
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+}
+
+// Output
+
+// You have 105 total messages in your inbox.
