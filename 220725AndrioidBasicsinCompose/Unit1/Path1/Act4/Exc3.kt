45211db0359819546c04c39853865522e76b7b45
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 20:57:27 +0600

    Create Exc3.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc3.kt
@@ -0,0 +1,16 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val count: Int = 2
+    println(count)
+    
+    val value: Int = 10
+    println(value)
+}
+
+// Output
+
+// You have 10 unread messages.
