c68063be2330b5db7c771277f3962b6a44284a0e
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 20:55:26 +0600

    Create Exc2.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc2.kt
@@ -0,0 +1,13 @@
+
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val count: Int = 2
+    println("You have $count unread messages.")
+}
+
+// Output
+// You have 2 unread messages.
