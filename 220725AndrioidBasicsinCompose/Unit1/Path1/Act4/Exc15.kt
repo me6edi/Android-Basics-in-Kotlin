c4295bbe38ad91bcdba6699ebc28717aea243ee3
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:39:28 +0600

    Create Exc15.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc15.kt
@@ -0,0 +1,14 @@
+
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val notificationsEnabled: Boolean = false
+    println("Are notifications enabled? " + notificationsEnabled)
+}
+
+// Output
+
+// Are notifications enabled? false
