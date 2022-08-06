0015eca523f843fe00a28177244d968797f33d60
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:41:25 +0600

    Create Exc16.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc16.kt
@@ -0,0 +1,22 @@
+
+/**
+ * Program Code
+ * Commenting in your code
+ */
+
+/**
+ * This program displays the number of messages
+ * in the user's inbox.
+ */
+fun main() {
+    // Create a variable for the number of unread messages.
+    var count = 10
+    println("You have $count unread messages.")
+
+    // Decrease the number of messages by 1.
+    count--
+    println("You have $count unread messages.")
+}
+ // Output
+// You have 10 unread messages.
+// You have 9 unread messages.
