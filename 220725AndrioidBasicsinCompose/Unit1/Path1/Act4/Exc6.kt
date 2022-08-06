1a2f5077b001b13b8190870af56502acc54f7133
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 21:19:10 +0600

    Create Exc6.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc6.kt
@@ -0,0 +1,14 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    var cartTotal = 0
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+
+// Output
+
+// Total: 20
