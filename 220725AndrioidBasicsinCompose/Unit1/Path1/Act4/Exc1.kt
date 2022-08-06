09f184f38eb6fbbecacb9fedb632c464a284e80a
Mehedi Amin <47064496+me6edi@users.noreply.github.com>
2022-08-06 20:52:41 +0600

    Create Exc1.kt

---
+++ 220725AndrioidBasicsinCompose/Unit1/Path1/Act4/Exc1.kt
@@ -0,0 +1,17 @@
+/**
+ * Program Code
+ * Create and use variables in Kotlin
+ */
+
+fun main() {
+    val count: Int = 2
+    println(count)
+    
+    val value: Int = 10
+    println(value)
+}
+
+// Output
+
+// 2
+// 10
